efb953f2b6bd9389142933f6bde09e0b311315de
project <project@DESKTOP-EAHHS5B>
2023-11-16 22:10:20 -0800

    Eng2 First Commit

---
+++ src/test/resources/TestCases/tc3Test.java
@@ -0,0 +1,12 @@
+package TestCases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class tc3Test {
+@Test
+public void m3()
+{
+	Reporter.log("m3 Running", true);
+}
+}
